e21579daa8ba06ebffc45602b52350adf13c76c6
Konstantinos Chousos <K.chousos@protonmail.com>
2025-04-19 12:58:35 +0300

    added more documentation to __init__.py for pdoc

--- src/llm_harness/__init__.py
+++ src/llm_harness/__init__.py
@@ -1,3 +1,11 @@
+"""
+<center>
+Automatically create harnesses for your C/C++ project using LLMs!
+</center>
+
+`llm_harness.harness_utils`: Functions to create harnesses.
+"""
+
 __all__ = ["harness_utils", "utils"]
 
 from . import harness_utils
