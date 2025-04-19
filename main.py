cf06adcb10bcf8887f264745ef39c40aec1f74df
Konstantinos Chousos <K.chousos@protonmail.com>
2025-04-19 13:12:01 +0300

    improved imports in main

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ Main function utilizing the llm_harness package
 """
 
 from loguru import logger
-import llm_harness as lh
+import llm_harness.utils as utils
+import llm_harness.harness_utils as lh
 
 
 def main() -> None:
@@ -13,18 +14,16 @@ def main() -> None:
     directory.
     """
 
-    project_path, model = lh.utils.parse_arguments()
+    project_path, model = utils.parse_arguments()
 
     logger.info("Reading project and collecting information...")
-    project_info = lh.utils.get_project_info(project_path)
+    project_info = utils.get_project_info(project_path)
 
     logger.info("Calling LLM to generate a harness...")
-    harness = lh.harness_utils.create_harness(
-        model=model, project_info=project_info
-    )
+    harness = lh.create_harness(model=model, project_info=project_info)
 
     logger.info("Writing harness to project...")
-    lh.harness_utils.write_harness(harness=harness, project_path=project_path)
+    lh.write_harness(harness=harness, project_path=project_path)
 
     logger.info("All done!")
 
